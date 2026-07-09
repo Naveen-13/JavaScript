d3ec947d55ced79a32f26678f4a8c887567c70cf
Naveen-13 <80417331+Naveen-13@users.noreply.github.com>
2026-07-09 15:23:22 +0000

    COMPLETED VARIABLE CHAPTER

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,4 +3,15 @@ let name = "Hitesh"
 var city = "pune"
 designation = "Developer"
 
-let 
+let salary; // HERE WE ONLY INTILIZED THE VARIABLE NOT DECLARED IT. SO IT DEFAULT VALUE WILL BE UNDEFINED
+
+name = "Naveen";
+
+/*
+    DONT USE THE VAR KEYWORD IN YOUR WORK AS IT DOES NOT WORK IN SCOPE.
+    MEANING IF I DECLARE SAME VARIABLE INSIDE A SCOPE { } LIKE FOR OR IF LOOP THEN THE OUTER VARIABLE VALUE ALSO CHANGES
+*/
+console.log(id);
+
+console.table([name, city, designation, salary]); // table fuction is used to print multiple variable at once
+
