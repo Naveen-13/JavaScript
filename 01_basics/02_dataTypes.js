bba0282a13f7da3005367c347e48186980c970a9
Naveen-13 <80417331+Naveen-13@users.noreply.github.com>
2026-07-12 08:38:09 +0000

    added

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+//Data types
+
+let age = 13;
+// Number
+// String
+// Boolean
+// bigInt
+// undefined
+// null -> standalone value
+// symbol -> to define unique
+
+console.log(typeof "hello");
+console.log(typeof age);
+console.log(typeof null); // object (INTERVIEW)
+console.log(typeof undefined); // undefined
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+let score = undefined;
+
+let value = Number(score);
+
+console.log(value);
+
+
+// "33" > 33
+// "33abc" > NaN (not a number)
+// "abc" > NaN
+// null > 0
+// undefined > Nan
+
+let bool = undefined;
+
+let intoBoolean = Boolean(bool);
+console.log(intoBoolean);
+
+// 1 > true
+// 0 > false
+// "" > false
+// "fdsf" > true
+// null > false
+// undefined > false
+
+//*******************************OPERATORS ***************************************************
+
+//log(2**3) means 2 raised to power 3
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2"); // 33
+console.log("1" + 2 + 2);  // 122
+
+//Read about prefix and postfix mdn
+
+
+
+
+
+
+
+
+
